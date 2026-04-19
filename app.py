ae90867a57ee429f54acb67ae4daf3a18a529071
Prajwal UR <prajwalurpraju@gmail.com>
2026-04-19 19:45:05 +0530

    Replace markdown image with st.image for display

--- app.py
+++ app.py
@@ -10,14 +10,8 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns([1.5, 5])
 
 with col1:
-    st.markdown(
-        """
-        <div style="margin-top: 25px;">
-            <img src="virat.jpg" width="160">
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown("<br><br>", unsafe_allow_html=True)  # pushes image down
+    st.image("virat.jpg", width=160)
 
 with col2:
     st.title("Virat Kohli ODI Performance Dashboard 🏏")
